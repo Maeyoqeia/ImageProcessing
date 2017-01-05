cbc907349c5be24728b11740489410017cd1d7c4
LifY05 <lif.aka.midna@googlemail.com>
2017-01-05 22:03:23 +0100

    alles fertig :D

--- P5/p05_filt2d.m
+++ P5/p05_filt2d.m
@@ -3,12 +3,12 @@ function img_filt = p05_filt2d(image,kernel)
 %Kernelgröße für Frequenzraum anpassen
 [m n] = size(image);
 [a b] = size(kernel);
-x_start = floor(m/2)-floor(a/2)
-x_end = floor(m/2)+floor(a/2)
-y_start = floor(n/2)-floor(b/2)
-y_end = floor(n/2)+floor(b/2)
+x_start = floor(m/2)-floor(a/2);
+x_end = floor(m/2)+floor(a/2);
+y_start = floor(n/2)-floor(b/2);
+y_end = floor(n/2)+floor(b/2);
 A = zeros(m,n);
-A(x_start:x_end, y_start:y_end) = fliplr(kernel);%fliplr siehe Hinweis Aufgabenteil 1 (weil Konvolution verlangt, keine Korrelation)
+A(x_start:x_end, y_start:y_end) = flipud(fliplr(kernel));%siehe Hinweis Aufgabenteil 1 (weil Konvolution verlangt, keine Korrelation)
 
 %Transformation in den Fourierraum
 imagefft = fft2(fftshift(image));
--- P5/p05_test.m
+++ P5/p05_test.m
@@ -5,14 +5,105 @@ close all; clear; clc;
 img = imread('p05_Bild01.bmp'); 
 image = im2double(img);
 
-%show picture
+%Originalbild
 figure(1); imshow(image);
+
+%a) Sobel-Filterkern
 a = [1 2 1]';
 b = [-1 0 1];
 sobelkernelx = a*b;
 sobelkernely = b'*a';
-img1 = p05_conv2d(image, sobelkernelx);
-img2 = p05_filt2d(image, sobelkernelx);
 
-figure(2); imshow(img1);
-figure(3); imshow(img2);
+%b) Mittelwert-Filterkern
+filter_M3 = 1/(3*3) * ones(3,3);
+filter_M7 = 1/(7*7) * ones(7,7);
+filter_M11 = 1/(11*11) * ones(11,11);
+
+% OR Funktionsaufruf und Zeitmessung
+start0 = tic;
+img0 = p05_conv2d(image, sobelkernelx);
+zeit0 = toc(start0);
+
+start1 = tic;
+img1 = p05_conv2d(image, sobelkernely);
+zeit1 = toc(start1);
+
+start2 = tic;
+img2 = p05_conv2d(image, filter_M3);
+zeit2 = toc(start2);
+
+start3 = tic;
+img3 = p05_conv2d(image, filter_M7);
+zeit3 = toc(start3);
+
+start4 = tic;
+img4 = p05_conv2d(image, filter_M11);
+zeit4 = toc(start4);
+
+% Ausgabe fuer Ortsraum
+figure(2);
+subplot(2,2,1);
+imshow(img0, []);
+title(['im OR gefiltert: Kanten (Sobel,x-Richtung) 3x3 ',char(10),'Die Berechnungsdauer betraegt: ',num2str(zeit0),' s']);
+subplot(2,2,2);
+imshow(img1, []);
+title(['im OR gefiltert: Kanten (Sobel,y-Richtung) 3x3 ',char(10),'Die Berechnungsdauer betraegt: ',num2str(zeit1),' s']);
+
+figure(3);
+subplot(2,3,1);
+imshow(img2, []);
+title(['im OR gefiltert: Glaettung (Mittelwert) 3x3 ',char(10),'Berechnungsdauer ',num2str(zeit2),' s']);
+subplot(2,3,2);
+imshow(img3, []);
+title(['im OR gefiltert: Glaettung (Mittelwert) 7x7 ',char(10),'Berechnungsdauer ',num2str(zeit3),' s']);
+subplot(2,3,3);
+imshow(img4, []);
+title(['im OR gefiltert: Glaettung (Mittelwert) 11x11 ',char(10),'Berechnungsdauer: ',num2str(zeit4),' s']);
+
+
+% FR Funktionsaufruf und Zeitmessung
+start0 = tic;
+img0 = p05_filt2d(image, sobelkernelx);
+zeit0 = toc(start0);
+
+start1 = tic;
+img1 = p05_filt2d(image, sobelkernely);
+zeit1 = toc(start1);
+
+start2 = tic;
+img2 = p05_filt2d(image, filter_M3);
+zeit2 = toc(start2);
+
+start3 = tic;
+img3 = p05_filt2d(image, filter_M7);
+zeit3 = toc(start3);
+
+start4 = tic;
+img4 = p05_filt2d(image, filter_M11);
+zeit4 = toc(start4);
+
+% Ausgabe fuer Frequenzraum
+figure(2);
+subplot(2,2,3);
+imshow(img0, []);
+title(['im FR gefiltert: Kanten (Sobel,x-Richtung) 3x3 ',char(10),'Berechnungsdauer: ',num2str(zeit0),' s']);
+subplot(2,2,4);
+imshow(img1, []);
+title(['im FR gefiltert: Kanten (Sobel,y-Richtung) 3x3 ',char(10),'Berechnungsdauer: ',num2str(zeit1),' s']);
+
+figure(3);
+subplot(2,3,4);
+imshow(img2, []);
+title(['im FR gefiltert: Glaettung (Mittelwert) 3x3 ',char(10),'Berechnungsdauer: ',num2str(zeit2),' s']);
+subplot(2,3,5);
+imshow(img3, []);
+title(['im FR gefiltert: Glaettung (Mittelwert) 7x7 ',char(10),'Berechnungsdauer: ',num2str(zeit3),' s']);
+subplot(2,3,6);
+imshow(img4, []);
+title(['im FR gefiltert: Glaettung (Mittelwert) 11x11 ',char(10),'Berechnungsdauer: ',num2str(zeit4),' s']);
+
+%Beoachtung: Entgegen unserer Erwartung, dass ein 3x3 Filterkern weniger Berechnungszeit benoetigt
+%als ein größerer Filterkern, zeigen mehrmalige Durchlaeufe, dass pro Durchlauf 3x3, 7x7 und 11x11 um die gleichen Werte 
+%schwanken. Dabei ist mal die Berechnung des 3x3, 7x7 oder 11x11 Kerns schneller.
+%Vermutlich zeigen sich deutliche Unterschiede erst bei wesentlich groesseren Filterkernen.
+%Die Filterung im Frequenzraum erfolgt durch die Multiplikation wesentlich schneller als durch Konvolution im Ortsraum.
