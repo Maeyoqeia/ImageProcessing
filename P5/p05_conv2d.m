c2fe59c89931baffe87bff8fb245e80509bad2fd
Viola Hauffe <Maeyoqeia@users.noreply.github.com>
2017-01-09 22:56:56 +0100

    Do last changes
    -added flipping in convolution
    -deleted flipping in filtering

--- P5/p05_conv2d.m
+++ P5/p05_conv2d.m
@@ -6,12 +6,10 @@ padding = floor(kernelsize/2);
 image_pddd = padarray(image, [padding padding]);
 kernel_flpd = flipud(fliplr(kernel));
 
-img_filt = ones(m,n);
-
 for x = 1+padding:m+padding
   for y = 1+padding:n+padding
   subA = image_pddd(x-padding:x+padding,y-padding:y+padding);
-  img_conv(x,y) = sum(sum(subA.*kernel));
+  img_conv(x,y) = sum(sum(subA.*kernel_flpd));
   end
  end
 
--- P5/p05_filt2d.m
+++ P5/p05_filt2d.m
@@ -8,11 +8,11 @@ x_end = floor(m/2)+floor(a/2);
 y_start = floor(n/2)-floor(b/2);
 y_end = floor(n/2)+floor(b/2);
 A = zeros(m,n);
-A(x_start:x_end, y_start:y_end) = flipud(fliplr(kernel));%siehe Hinweis Aufgabenteil 1 (weil Konvolution verlangt, keine Korrelation)
-
+A(x_start:x_end, y_start:y_end) = kernel;
 %Transformation in den Fourierraum
 imagefft = fft2(fftshift(image));
 kernelfft = fft2(fftshift(A));
+
 img_filtered = imagefft.*kernelfft;
 
 img_filt = ifftshift(ifft2(img_filtered));
